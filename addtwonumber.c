d230826275fbf53884c645963660b2b2c908bd50
Prabal-Srivastava <prabalrishu123@gmail.com>
2024-10-10 12:41:20 +0530

    add two number

---
+++ addtwonumber.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("first number = ");
+    scanf("%d",&a);
+    printf("second number = ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("The sum of two number = %d",c);
+    return 0;
+}
